1220132f2b4d5e2ca14432b9d211002b9b10dd89
alexandrefnsc <alexandre.fnsc@gmail.com>
2016-04-11 17:19:31 -0300

    first commit

---
+++ js/pessoal.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$('.content-all').load('home.html');
+
+	$('body').jKit();
+
+	var sobeHome = function(){
+		$(".btn-success").animate({"opacity":0}, 1000);
+		$('.text-page').animate({'opacity':0}, 1000,"easeInOutQuint");
+		$('.anima-01').animate({'top':-800}, 1000,"easeInExpo");
+		$('.anima-02').delay(100).animate({'top':-800}, 1000,"easeInExpo");
+		$('.parallax3').delay(200).animate({'top':300}, 1000,"easeInExpo");
+	}
+
+	// MENU
+	$('.hamburger-menu').click(function(event) {
+		$(this).animate({"right":"-20%"}, 1000,"easeInOutQuint");
+		$('.menu-hotsite').animate({"right":"0px"}, 1000,"easeInOutQuint");
+	});
+
+	$('.menu-hotsite li').click(function(event) {
+		$(".menu-hotsite").animate({"right":"-40%"}, 1000,"easeInOutQuint");
+		$('.hamburger-menu').animate({"right":"5%"}, 1000,"easeInOutQuint");
+	});
+
+	$('.menu-hotsite span').click(function(event) {
+		$(".menu-hotsite").animate({"right":"-40%"}, 1000,"easeInOutQuint");
+		$('.hamburger-menu').animate({"right":"5%"}, 1000,"easeInOutQuint");
+	});
+
+	$('#menu-home').click(function(event) {
+		sobeHome()
+		setTimeout(function(){ $('.content-all').load('home.html'); }, 1500);
+	});
+
+	$('#menu-startup').click(function(event) {
+		sobeHome()
+		setTimeout(function(){ $('.content-all').load('startups.html'); }, 1500);
+	});
+});
